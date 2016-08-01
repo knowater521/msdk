64006f41bc90ea4dea54456733616095e477598e
waterwindwood <469798015@qq.com>
2016-08-02 00:04:50 +0800

    完成对被控机的操作
    注意下python的64位加载64位的dll

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import time
+from ctypes import *
+api = cdll.LoadLibrary("msdk.dll")
+vid = api.M_Open(1)
+api.M_LeftClick(vid, 2)
+time.sleep(1)
+api.M_MoveTo(vid, 200, 200)
+time.sleep(5)
+api.M_ResetMousePos(vid)
