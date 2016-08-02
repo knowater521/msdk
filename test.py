77e4920cd81528104ddd9a868c3c5bd98401f38a
waterwindwood <469798015@qq.com>
2016-08-02 18:11:41 +0800

    修改为翻页的顺序

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import time
 from ctypes import *
 api = cdll.LoadLibrary("msdk.dll")
 vid = api.M_Open(1)
-api.M_LeftClick(vid, 2)
+api.M_ResetMousePos(vid)
 time.sleep(1)
 api.M_MoveTo(vid, 200, 200)
 time.sleep(5)
-api.M_ResetMousePos(vid)
+api.M_LeftClick(vid, 2)
+time.sleep(1)
